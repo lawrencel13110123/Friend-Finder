32d3538f36f35230a38f5e26b8d4e95e6788acc5
Liu <liu@LiutekiMacBook-Air.local>
2017-07-22 15:16:33 -0700

    completed project

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+// we put quotes around the keys because they are JSON objects 
+	{
+		"name": "Yoda", 
+		"img": "http://vignette2.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png", 
+		"answer": [
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5"
+		]
+	}
+]
+
+module.exports = friendsArray; 
